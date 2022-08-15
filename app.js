f2ed6f1fad6e13f29e16b43c23cf8e19dc637f10
Etienne Bissonnette <etiennebissonnette@Etiennes-MacBook-Pro.local>
2022-08-15 15:45:15 -0400

    Added CSS styles and work list

--- app.js
+++ app.js
@@ -4,34 +4,57 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 let items = [];
+let workItems = [];
 
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+app.use(express.static("Public"))
 
 app.listen(port, function () {
     console.log('listening on port' + port);
 });
 
+// GET /home
+
 app.get('/', function (req, res) {
     let options = {
         weekday: 'long',
-        year: 'numeric',
         month: 'long',
         day: 'numeric'
     };
 
     let today = new Date();
     res.render('list', {
-        currentDay: today.toLocaleDateString("en-US",options),
+        listTitle: today.toLocaleDateString("en-US", options),
         listItems: items,
     });
 });
 
+// GET /work
+
+app.get('/work', function (req, res) {
+    res.render('list', {
+        listTitle: "Work Task",
+        listItems: workItems,
+    });
+});
+
 app.post('/', function (req, res) {
-    item = req.body.newItem;
-    items.push(item);
-    res.redirect("/");
+
+    let item = req.body.newItem;
+    console.log(req.body)
+
+    if (req.body.list === "Work") {
+        workItems.push(item);
+        res.redirect("/work");
+    } else {
+        items.push(item);
+        res.redirect("/");
+        console.log(req.body)
+    }
 })
+
+
